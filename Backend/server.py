84dfb51dd584dce6d2538335343c5af050ec8ec8
Munch42 <41879159+Munch42@users.noreply.github.com>
2026-03-14 17:25:11 -0600

    Added basic hourly rate limiting functionality

--- Backend/server.py
+++ Backend/server.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+
+# This will return code 429 if the user is rate limited. The frontend can use this to display a clearer message if needed
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["40 per hour"],
+    storage_uri="memory://", # This is fine for our non-production storage
+)
 
 # Method to use to request questions to be generated
 # Takes in:
