b390982b10067908986c9e2ae0eea0caa33a04e4
Munch42 <41879159+Munch42@users.noreply.github.com>
2026-03-14 17:27:19 -0600

    Added minute rate limits to the LLM routes to conserve LLM API calls

--- Backend/server.py
+++ Backend/server.py
@@ -19,6 +19,7 @@ limiter = Limiter(
 # Then query the LLM and ask it to generate questions
 # Return the questions in the specified format for the front end
 @app.route('/api/questions', methods=['POST'])
+@limiter.limit("1 per minute") # Limit the LLM routes to once a minute to ensure that the limit API calls are conserved
 def questions():
     data = request.get_json()
     resumeText = data["resume"]
@@ -34,6 +35,7 @@ def questions():
 # User answers to each question
 # Then, it queries the LLM and asks it to generate feedback for these questions based on the user info
 @app.route('/api/feedback', methods=['POST'])
+@limiter.limit("1 per minute") # Limit the LLM routes to once a minute to ensure that the limit API calls are conserved
 def feedback():
     data = request.get_json()
     resumeText = data["resume"]
