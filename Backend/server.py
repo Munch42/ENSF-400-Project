8a4f9bddf908b08c1b87ed031c9e9372758eea6c
Munch42 <41879159+Munch42@users.noreply.github.com>
2026-03-14 17:10:25 -0600

    Added the basic Flask server setup. Added the /api/questions route

---
+++ Backend/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Method to use to request questions to be generated
+# Takes in:
+# User Resume
+# User Job Posting
+# Then query the LLM and ask it to generate questions
+# Return the questions in the specified format for the front end
+@app.route('/api/questions', methods=['POST'])
+def questions():
+    data = request.get_json()
+    return jsonify({"received": data}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
