c5a13a5a0441734ed49ddf0ed4b1f3a5b421360d
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-03-18 14:29:00 -0600

    Adds function for basic LLM call with fallbacks through LiteLLM

--- Backend/llm-calls.py
+++ Backend/llm-calls.py
@@ -1,8 +1,16 @@
-import os
 from dotenv import load_dotenv
+from litellm import completion
 
+# Load API key from 
 load_dotenv(override=True)
 
+MAIN_MODEL = "gemini/gemini-3-flash-preview"
+FALLBACKS = ["gemini/gemma-3-27b-it"]
+
+def llm_call(prompt: str):
+    messages = [{"role": "user", "content": prompt}]
+    response = completion(MAIN_MODEL, messages, fallbacks=FALLBACKS)
+    return response.choices[0].message.content  # type: ignore
+
 if __name__ == "__main__":
-    api_key = os.getenv("API_KEY")
-    print(api_key)
+    print(llm_call("Hello!"))
