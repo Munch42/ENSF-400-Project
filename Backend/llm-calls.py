47f1e22ff560b20d80151a74904dd9c22e88fd0b
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2026-03-18 14:57:54 -0600

    Adds parsing LLM responses to objects based on pydantic and litellm structured output

--- Backend/llm-calls.py
+++ Backend/llm-calls.py
@@ -1,16 +1,33 @@
 from dotenv import load_dotenv
 from litellm import completion
+from pydantic import BaseModel
+from typing import TypeVar
 
-# Load API key from 
+# Load API key(s) into the environment for LiteLLM
 load_dotenv(override=True)
 
+# Default model and fallbacks to use to generate responses
 MAIN_MODEL = "gemini/gemini-3-flash-preview"
-FALLBACKS = ["gemini/gemma-3-27b-it"]
+FALLBACKS = ["gemini/gemini-3.1-flash-lite-preview"]
 
-def llm_call(prompt: str):
+# Type of structured output schema
+S = TypeVar("S", bound=BaseModel)
+
+# Response schema to generate a list of questions
+class QuestionsList(BaseModel):
+    questions: list[str]
+
+def llm_call(prompt: str, **kwargs) -> str:
+    """Generate a response to `prompt` using `MAIN_MODEL` then `FALLBACK_MODELS`."""
     messages = [{"role": "user", "content": prompt}]
-    response = completion(MAIN_MODEL, messages, fallbacks=FALLBACKS)
+    response = completion(MAIN_MODEL, messages, fallbacks=FALLBACKS, **kwargs)
     return response.choices[0].message.content  # type: ignore
 
+def llm_call_structured_output(prompt: str, schema: type[S]):
+    """Generate a response to `prompt` in the format `schema` using `MAIN_MODEL` then `FALLBACK_MODELS`."""
+    response = llm_call(prompt, response_format=schema, enable_json_schema_validation=True)
+    return schema.model_validate_json(response)
+
+# Test llm call with structured output
 if __name__ == "__main__":
-    print(llm_call("Hello!"))
+    print(llm_call_structured_output("Hello!", QuestionsList))
