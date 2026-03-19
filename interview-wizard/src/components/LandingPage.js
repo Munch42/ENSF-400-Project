283e9449a85b2e759eb0c07a00ed4d3123ad460e
Tiffany P <tiffany.pham1@ucalgary.ca>
2026-03-18 23:36:50 -0600

    Completed first section of landing page

--- interview-wizard/src/components/LandingPage.js
+++ interview-wizard/src/components/LandingPage.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavigationRibbon from './NavigationRibbon';
+import styles from './LandingPage.module.css';
 
 const LandingPage = () => {
+    const navigate = useNavigate();
     return (
         <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
             <NavigationRibbon />
             <main>
+                <section className={styles.startSection}>
+                    <p style={{
+                        fontSize:'30px',
+                        marginTop:'0px',
+                        paddingBottom:'0px',
+                        textAlign:'center'}}>
+                        Get Interviewed by the Best AIs
+                    </p>
+                    <p style={{
+                        textAlign:'center', 
+                        marginTop:'0px', 
+                        marginBottom:'30px',
+                        paddingLeft:'50px', 
+                        paddingRight:'50px'}}>
+                        Leverage artificial intelligence to prepare for your most difficult interview in seconds!
+                    </p>
+                    <button type="button" onClick={() => navigate('/upload')} className={styles.startButton}>
+                        Get Started Now
+                    </button>
+                </section>
                 <h1>TODO: Complete Landing Page</h1>
             </main>
         </div>
