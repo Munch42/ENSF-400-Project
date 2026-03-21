d64feb383f250b31decc7259ca405d181b422634
Stephen Ravelo <stephenravelo31@gmail.com>
2026-03-20 18:14:46 -0600

    Install packages

---
+++ interview-wizard/src/components/ResumeUpload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResumeUpload = () => {
+    return (
+        <div>
+            <h1>TODO: Complete Resume Upload Component</h1>
+        </div>
+    );
+};
+
+export default ResumeUpload;
--- interview-wizard/src/components/UploadPage.js
+++ interview-wizard/src/components/UploadPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavigationRibbon from './NavigationRibbon';
+import ResumeUpload from './ResumeUpload';
 
 const UploadPage = () => {
     return (
         <div>
             <NavigationRibbon />
             <h1>TODO: Complete Upload Page</h1>
+            <ResumeUpload />
         </div>
     );
 };
