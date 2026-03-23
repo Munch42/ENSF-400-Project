d410a8eecb0a475f47f8afe0a2fdf43b1b6866a8
Munch42 <41879159+Munch42@users.noreply.github.com>
2026-03-22 23:01:30 -0600

    Updated the backend URLs to match the remotely deployed version

--- interview-wizard/src/components/ExportPage.js
+++ interview-wizard/src/components/ExportPage.js
@@ -19,7 +19,7 @@ const ExportPage = () => {
 
         const fetchFeedback = async () => {
             try {
-                const response = await fetch('http://localhost:5000/api/feedback', {
+                const response = await fetch('https://ensf-400-project.onrender.com//api/feedback', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({
--- interview-wizard/src/components/UploadPage.js
+++ interview-wizard/src/components/UploadPage.js
@@ -26,7 +26,7 @@ const UploadPage = () => {
     const sendQuestionRequest = async (resumeText, jobPostingText) => {
         setLoading(true);
         try {
-            const response = await fetch("http://localhost:5000/api/questions", {
+            const response = await fetch("https://ensf-400-project.onrender.com/api/questions", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
